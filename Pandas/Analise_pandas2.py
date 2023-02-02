2d05056b276c168a848af1e1f28c6f140e0e4091
Leonardo Henke <98836382+LeonardoHenke@users.noreply.github.com>
2023-02-02 14:21:31 -0300

    Update Analise_pandas2.py

--- Pandas/Analise_pandas2.py
+++ Pandas/Analise_pandas2.py
@@ -11,19 +11,17 @@ df_vendas2017 = pd.read_csv(r'E:\Programas\Projetos-Python\Pandas\Contoso - Vend
 # tirar as colunas escolhidas como argumentos. Usando axis=0 para linhas and axis=1 for colunas 
 ## dt_clientes = dt_clientes.drop(['Unnamed: 7', 'Unnamed: 8', 'Unnamed: 9', 'Unnamed: 10'], axis=1)
 
-# pegar apenas as colunas desejadas como argumento da tabela clientes
+# pegar apenas as colunas desejadas como argumento
 coluna_desejada_clientes = ['ID Cliente', 'E-mail']
 df_clientes = df_clientes[coluna_desejada_clientes]
 
-# pegar apenas as colunas desejadas como argumento da tabela produtos
 coluna_desejada_produtos = ['ID Produto', 'Nome da Marca']
 df_produtos = df_produtos[coluna_desejada_produtos]
 
-# pegar apenas as colunas desejadas como argumento da tabela lojas
 coluna_desejada_lojas = ['ID Loja', 'Nome da Loja']
 df_lojas = df_lojas[coluna_desejada_lojas]
 
-# agrupar os dataframes desejados
+# agrupar os dataframes desejados. Precisamos que o dataframe base tenha uma coluna que seja igual ao outro dataframe que sera adicionado, so assim ele vai identificar as informacoes certas nas linhas certas
 df_vendas2017 = df_vendas2017.merge(df_produtos, on='ID Produto')
 df_vendas2017 = df_vendas2017.merge(df_clientes, on='ID Cliente')
 df_vendas2017 = df_vendas2017.merge(df_lojas, on='ID Loja')
@@ -34,7 +32,7 @@ df_vendas2017 = df_vendas2017.rename(columns={'E-mail' : 'E-mail do Cliente'})
 
 
 
-print(df_vendas2017[:4])
+print(df_vendas2017)
 #print(dt_clientes[:4])
 #print(df_produtos[:4])
 #print(df_lojas[:4])
