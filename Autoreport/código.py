e414bec4236596a4989198a7467efce654648707
Leonardo Henke <98836382+LeonardoHenke@users.noreply.github.com>
2023-02-08 10:13:20 -0300

    auto reporte lol

---
+++ Autoreport/código.py
@@ -0,0 +1,71 @@
+import pyautogui
+from time import sleep
+
+sleep(2)
+
+# - clicar em continuar:
+#pyautogui.click(815,792, duration=0.5)
+# - Primeiro jogador:
+#     - clicar em denunciar jogador
+pyautogui.click(577,288, duration=0.5)
+#     - clicar em feeding intencional
+pyautogui.click(782,487, duration=0.5)
+#     - clicar em abuso verbal
+pyautogui.click(775,408, duration=0.5)
+#     - clicar em atitude negativa
+pyautogui.click(775,362, duration=0.5)
+#     - clicar em denunciar:
+pyautogui.click(905,724, duration=0.5)
+
+
+# - Segundo jogador:
+#     - clicar em denunciar jogador
+pyautogui.click(579,329, duration=0.5)
+#     - clicar em feeding intencional
+pyautogui.click(782,487, duration=0.5)
+#     - clicar em abuso verbal
+pyautogui.click(775,408, duration=0.5)
+#     - clicar em atitude negativa
+pyautogui.click(775,362, duration=0.5)
+#     - clicar em denunciar:
+pyautogui.click(905,724, duration=0.5)
+
+# - Terceiro jogador:
+#     - clicar em denunciar jogador
+pyautogui.click(576,369, duration=0.5)
+#     - clicar em feeding intencional
+pyautogui.click(782,487, duration=0.5)
+#     - clicar em abuso verbal
+pyautogui.click(775,408, duration=0.5)
+#     - clicar em atitude negativa
+pyautogui.click(775,362, duration=0.5)
+#     - clicar em denunciar:
+pyautogui.click(905,724, duration=0.5)
+
+
+# - Quarto jogador:
+#     - clicar em denunciar jogador
+pyautogui.click(576,406, duration=0.5)
+#     - clicar em feeding intencional
+pyautogui.click(782,487, duration=0.5)
+#     - clicar em abuso verbal
+pyautogui.click(775,408, duration=0.5)
+#     - clicar em atitude negativa
+pyautogui.click(775,362, duration=0.5)
+#     - clicar em denunciar:
+pyautogui.click(905,724, duration=0.5)
+
+
+# - Quinto jogador:
+#     - clicar em denunciar jogador
+pyautogui.click(576,449, duration=0.5)
+#     - clicar em feeding intencional
+pyautogui.click(782,487, duration=0.5)
+#     - clicar em abuso verbal
+pyautogui.click(775,408, duration=0.5)
+#     - clicar em atitude negativa
+pyautogui.click(775,362, duration=0.5)
+#     - clicar em denunciar:
+pyautogui.click(905,724, duration=0.5)
+# - clicar em continuar:
+pyautogui.click(834,798, duration=0.5)
