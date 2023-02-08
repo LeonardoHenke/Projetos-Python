261b15e4d35ce6a2a106945dc86a2d7c668e540e
Leonardo Henke <98836382+LeonardoHenke@users.noreply.github.com>
2023-02-08 10:14:07 -0300

    Update código.py

--- Autoreport/código.py
+++ Autoreport/código.py
@@ -4,68 +4,68 @@ from time import sleep
 sleep(2)
 
 # - clicar em continuar:
-#pyautogui.click(815,792, duration=0.5)
+#pyautogui.click(815,792, duration=0.8)
 # - Primeiro jogador:
 #     - clicar em denunciar jogador
-pyautogui.click(577,288, duration=0.5)
+pyautogui.click(577,288, duration=0.8)
 #     - clicar em feeding intencional
-pyautogui.click(782,487, duration=0.5)
+pyautogui.click(782,487, duration=0.8)
 #     - clicar em abuso verbal
-pyautogui.click(775,408, duration=0.5)
+pyautogui.click(775,408, duration=0.8)
 #     - clicar em atitude negativa
-pyautogui.click(775,362, duration=0.5)
+pyautogui.click(775,362, duration=0.8)
 #     - clicar em denunciar:
-pyautogui.click(905,724, duration=0.5)
+pyautogui.click(905,724, duration=0.8)
 
 
 # - Segundo jogador:
 #     - clicar em denunciar jogador
-pyautogui.click(579,329, duration=0.5)
+pyautogui.click(579,329, duration=0.8)
 #     - clicar em feeding intencional
-pyautogui.click(782,487, duration=0.5)
+pyautogui.click(782,487, duration=0.8)
 #     - clicar em abuso verbal
-pyautogui.click(775,408, duration=0.5)
+pyautogui.click(775,408, duration=0.8)
 #     - clicar em atitude negativa
-pyautogui.click(775,362, duration=0.5)
+pyautogui.click(775,362, duration=0.8)
 #     - clicar em denunciar:
-pyautogui.click(905,724, duration=0.5)
+pyautogui.click(905,724, duration=0.8)
 
 # - Terceiro jogador:
 #     - clicar em denunciar jogador
-pyautogui.click(576,369, duration=0.5)
+pyautogui.click(576,369, duration=0.8)
 #     - clicar em feeding intencional
-pyautogui.click(782,487, duration=0.5)
+pyautogui.click(782,487, duration=0.8)
 #     - clicar em abuso verbal
-pyautogui.click(775,408, duration=0.5)
+pyautogui.click(775,408, duration=0.8)
 #     - clicar em atitude negativa
-pyautogui.click(775,362, duration=0.5)
+pyautogui.click(775,362, duration=0.8)
 #     - clicar em denunciar:
-pyautogui.click(905,724, duration=0.5)
+pyautogui.click(905,724, duration=0.8)
 
 
 # - Quarto jogador:
 #     - clicar em denunciar jogador
-pyautogui.click(576,406, duration=0.5)
+pyautogui.click(576,406, duration=0.8)
 #     - clicar em feeding intencional
-pyautogui.click(782,487, duration=0.5)
+pyautogui.click(782,487, duration=0.8)
 #     - clicar em abuso verbal
-pyautogui.click(775,408, duration=0.5)
+pyautogui.click(775,408, duration=0.8)
 #     - clicar em atitude negativa
-pyautogui.click(775,362, duration=0.5)
+pyautogui.click(775,362, duration=0.8)
 #     - clicar em denunciar:
-pyautogui.click(905,724, duration=0.5)
+pyautogui.click(905,724, duration=0.8)
 
 
 # - Quinto jogador:
 #     - clicar em denunciar jogador
-pyautogui.click(576,449, duration=0.5)
+pyautogui.click(576,449, duration=0.8)
 #     - clicar em feeding intencional
-pyautogui.click(782,487, duration=0.5)
+pyautogui.click(782,487, duration=0.8)
 #     - clicar em abuso verbal
-pyautogui.click(775,408, duration=0.5)
+pyautogui.click(775,408, duration=0.8)
 #     - clicar em atitude negativa
-pyautogui.click(775,362, duration=0.5)
+pyautogui.click(775,362, duration=0.8)
 #     - clicar em denunciar:
-pyautogui.click(905,724, duration=0.5)
+pyautogui.click(905,724, duration=0.8)
 # - clicar em continuar:
-pyautogui.click(834,798, duration=0.5)
+pyautogui.click(834,798, duration=0.8)
