179c1cdf0e0ef2a74ef3565a6cf9c1ab7a8e82b2
Leonardo Henke <98836382+LeonardoHenke@users.noreply.github.com>
2023-02-28 11:34:23 -0300

    atts integração python-arq

--- Auto-report LOL/código.py
+++ Auto-report LOL/código.py
@@ -1,79 +1,32 @@
 import pyautogui
 import pygetwindow as gw
 from time import sleep
+import mods
 
+cordenadas_botaoDenunc = [[600, 336], [601, 373], [601, 414], [601, 459], [602, 496]]
+cordenadas_regiao = [[511, 318, 80, 23], [511, 358, 80, 23], [511, 399, 80, 23], [511, 441, 80, 23], [511, 482, 80, 23]]
+
+
+# abrir a janela do lol
 lol_window = gw.getWindowsWithTitle('League of Legends')[0]
 lol_window.activate()
 
+# pular honras:
+mods.botao_pular_honras()
 
-# - pular honras:
-pyautogui.click(961,808, duration=0.5)
-
-sleep(6)
+# esperando carregar a página
+sleep(4)
 
 # - clicar em continuar:
-pyautogui.click(825, 834, duration=0.2)
-# - Primeiro jogador:
-#     - clicar em denunciar jogador
-pyautogui.click(600, 336, duration=0.8)
-#     - clicar em feeding intencional
-pyautogui.click(804, 412, duration=0.2)
-#     - clicar em abuso verbal
-pyautogui.click(805, 501, duration=0.2)
-#     - clicar em atitude negativa
-pyautogui.click(804, 531, duration=0.2)
-#     - clicar em denunciar:
-pyautogui.click(933, 778, duration=0.2)
+mods.botao_continuar()
 
+# esperando carregar a página
+sleep(2)
 
-# - Segundo jogador:
-#     - clicar em denunciar jogador
-pyautogui.click(601, 373, duration=0.2)
-#     - clicar em feeding intencional
-pyautogui.click(804, 412, duration=0.2)
-#     - clicar em abuso verbal
-pyautogui.click(805, 501, duration=0.2)
-#     - clicar em atitude negativa
-pyautogui.click(804, 531, duration=0.2)
-#     - clicar em denunciar:
-pyautogui.click(933, 778, duration=0.2)
+for i, regiao in enumerate(cordenadas_regiao):
+    if pyautogui.locateOnScreen('nickname.png', region=(regiao), grayscale=True, confidence=0.4) == None:
+        pyautogui.click(cordenadas_botaoDenunc[i], duration=0.2)
+        mods.denuncias_do_jogador()
 
-# - Terceiro jogador:
-#     - clicar em denunciar jogador
-pyautogui.click(601, 414, duration=0.2)
-#     - clicar em feeding intencional
-pyautogui.click(804, 412, duration=0.2)
-#     - clicar em abuso verbal
-pyautogui.click(805, 501, duration=0.2)
-#     - clicar em atitude negativa
-pyautogui.click(804, 531, duration=0.2)
-#     - clicar em denunciar:
-pyautogui.click(933, 778, duration=0.2)
-
-
-# - Quarto jogador:
-#     - clicar em denunciar jogador
-pyautogui.click(601, 459, duration=0.2)
-#     - clicar em feeding intencional
-pyautogui.click(804, 412, duration=0.2)
-#     - clicar em abuso verbal
-pyautogui.click(805, 501, duration=0.2)
-#     - clicar em atitude negativa
-pyautogui.click(804, 531, duration=0.2)
-#     - clicar em denunciar:
-pyautogui.click(933, 778, duration=0.2)
-
-
-# - Quinto jogador:
-#     - clicar em denunciar jogador
-pyautogui.click(602, 496, duration=0.2)
-#     - clicar em feeding intencional
-pyautogui.click(804, 412, duration=0.2)
-#     - clicar em abuso verbal
-pyautogui.click(805, 501, duration=0.2)
-#     - clicar em atitude negativa
-pyautogui.click(804, 531, duration=0.2)
-#     - clicar em denunciar:
-pyautogui.click(933, 778, duration=0.2)
 # - clicar em continuar:
-pyautogui.click(825, 834, duration=0.2)
+#pyautogui.click(825, 834, duration=0.2)
